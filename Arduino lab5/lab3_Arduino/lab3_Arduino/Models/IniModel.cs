8d4446992a3fadaac348eec863a26709adeefd9a
2bigballs <illya.melnik.2030@gmail.com>
2023-11-25 15:14:00 +0200

    finish Task 5

---
+++ Arduino lab5/lab3_Arduino/lab3_Arduino/Models/IniModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_Arduino.Models
+{
+    public class IniModel
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
